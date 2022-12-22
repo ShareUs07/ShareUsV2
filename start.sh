0dace37b805e32a7dc50ea132bfe11260647c3de
ShareUs07 <121240148+ShareUs07@users.noreply.github.com>
2022-12-22 19:46:21 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo...."
-git clone https://github.com/ShareUs07/ShareUs07V2.git /MdiskSearchBotV2
-cd /MdiskSearchBotV2
+git clone https://github.com/ShareUs07/ShareUs07V2.git /ShareUs07BotV2
+cd /ShareUs07V2
 pip3 install -r requirements.txt
 echo "Starting Bot...."
 python3 main.py
